d67d8728449b4423f7d54e7b3ca2ef61b312caab
ruroru <111705692+ruroru@users.noreply.github.com>
2025-07-15 00:15:19 +0000

    Stepping project version to prepare it for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/embedded-mariadb-clj "1.0.4-SNAPSHOT"
+(defproject org.clojars.jj/embedded-mariadb-clj "1.1.0"
   :description "Embedded maria db for clojure"
   :url "https://github.com/ruroru/mariadb-embedded-clj"
   :license {:name "EPL-2.0"
