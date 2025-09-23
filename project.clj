df04b8332a05f87b16b96be2635df863b1d3d88b
jj <keistaslogairkosulys@gmail.com>
2025-09-23 22:50:20 +0300

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,18 +4,18 @@
   :license {:name "EPL-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[ch.vorburger.mariaDB4j/mariaDB4j "3.2.0"]
-                 [org.clojure/clojure "1.12.1"]
+                 [org.clojure/clojure "1.12.2"]
                  [org.clojure/tools.logging "1.3.0"]]
   :deploy-repositories [["clojars" {:url      "https://repo.clojars.org"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass}]]
   :profiles {:test {:global-vars    {*warn-on-reflection* true}
                     :resource-paths ["test/resources"]
-                    :dependencies   [[babashka/fs "0.5.26"]
+                    :dependencies   [
                                      [ch.qos.logback/logback-classic "1.5.18"]
-                                     [com.github.seancorfield/next.jdbc "1.3.1048"]
+                                     [com.github.seancorfield/next.jdbc "1.3.1070"]
                                      [mock-clj "0.2.1"]
-                                     [org.mariadb.jdbc/mariadb-java-client "3.5.4"]]}}
+                                     [org.mariadb.jdbc/mariadb-java-client "3.5.6"]]}}
   :plugins [[lein-ancient "0.7.0"]
             [org.clojars.jj/bump-md "1.0.0"]
             [org.clojars.jj/strict-check "1.0.2"]
